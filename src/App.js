cd6b2c962508fac514ebb2e83af59e5fc81a9664
YorKnEz <alexmitreanu04@gmail.com>
2022-05-05 22:13:37 +0300

    React App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from "react"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { ThemeContext, themes, UserContext } from "./context"
+
+import { Header, Footer, ProtectedRoute } from "./components"
+import { Home, News, SignUp, SignIn, Profile, BecomeEditor } from "./pages"
+
+export default function App() {
+	const [theme, setTheme] = useState(
+		themes[localStorage.getItem("theme") || "light"]
+	)
+	const [themeName, setThemeName] = useState(localStorage.getItem("theme"))
+	const [token, setToken] = useState(localStorage.getItem("token") || "")
+	const [user, setUser] = useState(
+		JSON.parse(localStorage.getItem("user")) || {}
+	)
+
+	// https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+	let vh = window.innerHeight * 0.01
+	document.documentElement.style.setProperty("--vh", `${vh}px`)
+	window.addEventListener("resize", () => {
+		let vh = window.innerHeight * 0.01
+		document.documentElement.style.setProperty("--vh", `${vh}px`)
+	})
+
+	// set a property for each color in the theme object
+	useEffect(() => {
+		for (const color in theme) {
+			// example:
+			// for entry "text: "#fff"", the property name will be "--text-color"
+			document.documentElement.style.setProperty(
+				`--${color}-color`,
+				`${theme[color]}`
+			)
+		}
+	}, [theme])
+
+	const toggleTheme = () => {
+		if (theme === themes.light) {
+			setTheme(themes.dark)
+			setThemeName("dark")
+			localStorage.setItem("theme", "dark")
+		} else {
+			setTheme(themes.light)
+			setThemeName("light")
+			localStorage.setItem("theme", "light")
+		}
+	}
+
+	const signIn = ({ token, user }) => {
+		setToken(token)
+		setUser(user)
+		localStorage.setItem("token", token)
+		localStorage.setItem("user", JSON.stringify(user))
+	}
+
+	const signOut = () => {
+		setToken("")
+		setUser({})
+		localStorage.removeItem("token")
+		localStorage.removeItem("user")
+	}
+
+	return (
+		<ThemeContext.Provider
+			value={{
+				theme,
+				toggleTheme,
+			}}
+		>
+			<UserContext.Provider
+				value={{
+					token,
+					user,
+					signIn,
+					signOut,
+				}}
+			>
+				<Router>
+					<Header theme={themeName} signOut={signOut} />
+					<Routes>
+						<Route exact path="/" element={<Home />} />
+						<Route exact path="/become-editor" element={<BecomeEditor />} />
+						<Route exact path="/sign-up" element={<SignUp />} />
+						<Route exact path="/sign-in" element={<SignIn signIn={signIn} />} />
+
+						<Route
+							exact
+							path="/news/:newsId"
+							element={
+								<ProtectedRoute>
+									<News />
+								</ProtectedRoute>
+							}
+						/>
+						<Route
+							exact
+							path="/profile"
+							element={
+								<ProtectedRoute>
+									<Profile />
+								</ProtectedRoute>
+							}
+						/>
+					</Routes>
+					<Footer />
+				</Router>
+			</UserContext.Provider>
+		</ThemeContext.Provider>
+	)
 }
-
-export default App;
