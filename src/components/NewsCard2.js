6e23e6a86c3a8696145b0c23e366519c2d9f732f
YorKnEz <alexmitreanu04@gmail.com>
2022-07-11 09:02:34 +0300

    Created NewsCard2 to display on author profiles

---
+++ src/components/NewsCard2.js
@@ -0,0 +1,67 @@
+import React, { useEffect } from "react"
+import { Link } from "react-router-dom"
+import { formatDistance, fromUnixTime } from "date-fns"
+
+import "./NewsCard2.scss"
+
+function NewsCard2({ data }) {
+	const showDate = () => {
+		const updatedAt = fromUnixTime(data.updatedAt / 1000)
+		const currentDate = fromUnixTime(Date.now() / 1000)
+		const distance = formatDistance(updatedAt, currentDate)
+
+		return data.createdAt === data.updatedAt
+			? `Posted ${distance} ago`
+			: `Edited ${distance} ago`
+	}
+
+	useEffect(() => {
+		const div = document.getElementById(data.id)
+
+		let body = data.body
+		// find the last line break to get exactly 5 lines to display
+
+		// replace all html tags from Draft-js to get raw text body
+		body = body.replaceAll(/<\/?[\s\S]*?>/g, "")
+
+		// get first 400 chars of that
+		body = body.slice(0, 396) + "..."
+
+		// console.log(body)
+
+		// render it
+		div.innerHTML = body
+	}, [data])
+
+	const showTags = () => {
+		// split the data.tags into an array of tags
+		let tags = data.tags.split(",")
+
+		return tags.map(s => (
+			<span className="tags_item" key={s}>
+				{s}
+			</span>
+		))
+	}
+
+	return (
+		<Link to={`/news/${data.id}`} className="news_card2">
+			{/* <div className="news_card2_overlay" /> */}
+			<div
+				className="news_card2_thumbnail"
+				style={{ backgroundImage: `url("${data.thumbnail}")` }}
+			/>
+			<div className="news_card2_info">
+				<span className="news_card2_info_title">{data.title}</span>
+
+				<div className="news_card2_info_body" id={data.id}></div>
+
+				<div className="news_card2_info_wrapper">
+					<div className="news_card2_info_tags">{showTags()}</div>
+					<p className="news_card2_info_date">{showDate()}</p>
+				</div>
+			</div>
+		</Link>
+	)
+}
+export default NewsCard2
