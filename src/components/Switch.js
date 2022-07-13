da24bb94d23e0a4fad778278d1b650bc5e05e208
YorKnEz <alexmitreanu04@gmail.com>
2022-07-13 16:28:15 +0300

    Improved theme switch

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -3,16 +3,19 @@ import React from "react"
 import "./Switch.scss"
 
 function Switch({ theme, toggleTheme, switchState, setSwitchState }) {
+	const handleClick = e => {
+		e.preventDefault()
+
+		toggleTheme()
+		setSwitchState(!switchState)
+	}
+
 	return (
-		<div className="switch">
+		<div className="switch" onClick={handleClick}>
 			<div
 				id="switchElement"
 				className="switch_circle"
 				style={theme === "dark" ? { left: "calc(36px - 16px)" } : { left: "0" }}
-				onClick={e => {
-					toggleTheme()
-					setSwitchState(!switchState)
-				}}
 			></div>
 		</div>
 	)
