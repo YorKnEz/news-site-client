f487adb53767f9528693efec820e05321ed604f0
YorKnEz <alexmitreanu04@gmail.com>
2022-07-19 07:42:56 +0300

    Added not found messages for news and searches

--- src/components/QueryResult.js
+++ src/components/QueryResult.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { AiOutlineFrown } from "react-icons/ai"
 import { useNavigate } from "react-router"
 
 import { SpinnerCircular } from "spinners-react"
@@ -29,6 +30,36 @@ function QueryResult({ loading, error, data, children }) {
 	}
 
 	if (data) {
+		// object to return if the array of news is empty
+		const notFound = (
+			<div
+				style={{
+					display: "flex",
+					alignItems: "center",
+					justifyContent: "center",
+					margin: "40px 0",
+					fontSize: "20px",
+				}}
+			>
+				<AiOutlineFrown />
+				<p style={{ marginLeft: "16px" }}>
+					You reached the bottom of the page.
+				</p>
+			</div>
+		)
+
+		// check for each array if it is empty and return notFound if so
+		if (data.newsForHome && data.newsForHome.length === 0) {
+			return notFound
+		}
+		if (data.newsForRedditHome && data.newsForRedditHome.length === 0) {
+			return notFound
+		}
+		if (data.newsForProfile && data.newsForProfile.length === 0) {
+			return notFound
+		}
+
+		// otherwise return the children
 		return children
 	}
 }
