2209ea417b8d9169be0600c6c67d1ae8d86f2ffa
YorKnEz <alexmitreanu04@gmail.com>
2022-07-29 10:12:13 +0300

    Added link color to themes for better usability

--- src/context/theme-context.js
+++ src/context/theme-context.js
@@ -1,5 +1,11 @@
 import React from "react"
 
+/*
+	OLD
+	link: "#44b6eb",
+	linkHover: "#75c9f0",
+*/
+
 export const themes = {
 	dark: {
 		text: "#fff",
@@ -10,6 +16,10 @@ export const themes = {
 		border: "#303030",
 		primary: "lightseagreen",
 		shadow: "black",
+		link: "rgb(27, 145, 204)",
+		linkBg: "rgba(27, 145, 204, 0.3)",
+		linkHover: "rgb(97, 188, 234)",
+		linkHoverBg: "rgb(97, 188, 234, 0.3)",
 	},
 	light: {
 		text: "#161616",
@@ -20,6 +30,10 @@ export const themes = {
 		border: "#a0a0a0",
 		primary: "lightseagreen",
 		shadow: "#161616",
+		link: "rgb(27, 145, 204)",
+		linkBg: "rgba(27, 145, 204, 0.3)",
+		linkHover: "rgb(97, 188, 234)",
+		linkHoverBg: "rgb(97, 188, 234, 0.3)",
 	},
 }
 
