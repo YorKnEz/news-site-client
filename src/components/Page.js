c002953c6b534877db58c462fbe796f42064ee39
Alex Mitreanu <alexmitreanu04@gmail.com>
2022-08-11 18:44:34 +0300

    Modified Page component to display the profile pages header when required

--- src/components/Page.js
+++ src/components/Page.js
@@ -1,13 +1,34 @@
-import React from "react"
+import React, { useContext } from "react"
+import { useParams } from "react-router"
+
+import { useQuery } from "@apollo/client"
 
 import "./Page.scss"
 import { Header, Footer } from "../components"
+import { UserContext } from "../context"
+import { USER } from "../utils/apollo-queries"
 
 function Page({ children }) {
+	const { id } = useParams()
+	const { user } = useContext(UserContext)
+
+	const { data } = useQuery(USER, {
+		variables: { id },
+		skip: !id,
+	})
+
+	const profileHeader = window.location.pathname.includes("profile")
+
+	const userData = profileHeader ? data?.user : user
+
 	return (
 		<>
-			<Header />
-			<div className="page">{children}</div>
+			<Header userData={userData} profileHeader={profileHeader} />
+			<div
+				className={`page ${profileHeader && userData ? "page_margin-top" : ""}`}
+			>
+				{children}
+			</div>
 			<Footer />
 		</>
 	)
