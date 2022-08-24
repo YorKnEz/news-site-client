6dc5bbe275747e568d157d94588f9d9fc2072534
Alex Mitreanu <alexmitreanu04@gmail.com>
2022-08-24 10:24:30 +0300

    Added followed pages for news only from followed authors

--- src/App.js
+++ src/App.js
@@ -148,9 +148,9 @@ export default function App() {
 							path="/verify-email/:token"
 							element={<VerifyEmail />}
 						/>
-						<Route exact path="/" element={<Feed sortBy="score" />} />
-						<Route exact path="/best" element={<Feed sortBy="score" />} />
-						<Route exact path="/new" element={<Feed sortBy="date" />} />
+						<Route exact path="/" element={<Feed />} />
+						<Route exact path="/best" element={<Feed />} />
+						<Route exact path="/new" element={<Feed />} />
 						<Route exact path="/r/romania" element={<Reddit />} />
 
 						<Route exact path="/news/:link-:newsId" element={<News />} />
@@ -162,6 +162,9 @@ export default function App() {
 
 						{/* private routes, accessible by all users */}
 						<Route element={<PrivateRoutes />}>
+							<Route exact path="/followed/" element={<Feed />} />
+							<Route exact path="/followed/best" element={<Feed />} />
+							<Route exact path="/followed/new" element={<Feed />} />
 							<Route
 								exact
 								path="/profile/:id/overview/"
